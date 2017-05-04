7113ebf539d8104b21c6b42e55e8635444403722
hansvb_immalle <hans.vanbroeckhoven@immalle.be>
2017-05-04 12:57:03 +0200

    Lijst van personen maken en afbeelden

---
+++ Javascript/index.js
@@ -0,0 +1,16 @@
+﻿console.log('hello');
+
+/*
+naam;leeftijd
+joske;13
+jantje;14
+mieke;15
+*/
+
+var personen = [];
+
+personen.push({ naam: "joske", leeftijd: 13 });
+personen.push({ naam: "jantje", leeftijd: 14 });
+personen.push({ naam: "mieke", leeftijd: 15 });
+
+personen.forEach(p => console.log(p.naam + " is " + p.leeftijd));

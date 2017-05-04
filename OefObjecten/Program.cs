3a697479658dc6c37f6cddc6b6738c48cc233959
hansvb_immalle <hans.vanbroeckhoven@immalle.be>
2017-05-04 13:03:09 +0200

    Lijst van personen in C#

--- OefObjecten/Program.cs
+++ OefObjecten/Program.cs
@@ -10,6 +10,17 @@ namespace OefObjecten
     {
         static void Main(string[] args)
         {
+            var personen = new List<Persoon>()
+            {
+                new Persoon { Naam = "Joske", Leeftijd = 13 },
+                new Persoon { Naam = "Jantje", Leeftijd = 14 },
+                new Persoon { Naam = "Mieke", Leeftijd = 15 }
+            };
+
+            foreach(var p in personen)
+            {
+                Console.WriteLine(p.Naam + " is " + p.Leeftijd);
+            }
         }
     }
 }
